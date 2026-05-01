143175a4d9abf5d066ab0131e1358dd7b9a2a66f
Szabó Béla Zoltán <szabobelazoltan@gmail.com>
2026-05-01 05:52:32 +0200

    Initial commit

---
+++ src/main/java/hu/szbz/das/DasApplication.java
@@ -0,0 +1,13 @@
+package hu.szbz.das;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DasApplication.class, args);
+	}
+
+}
