9c24af9f51175c0a51b24e030444d8b6467d6d47
Szabó Béla Zoltán <szabobelazoltan@gmail.com>
2026-05-01 19:14:17 +0200

    Entry point class has been renamed

--- src/main/java/hu/szbz/das/DailyActivitiesSpringApplication.java
+++ src/main/java/hu/szbz/das/DailyActivitiesSpringApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DasApplication {
+public class DailyActivitiesSpringApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DasApplication.class, args);
+		SpringApplication.run(DailyActivitiesSpringApplication.class, args);
 	}
 
 }
--- src/test/java/hu/szbz/das/DailyActivitiesSpringApplicationTests.java
+++ src/test/java/hu/szbz/das/DailyActivitiesSpringApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DasApplicationTests {
+class DailyActivitiesSpringApplicationTests {
 
 	@Test
 	void contextLoads() {
